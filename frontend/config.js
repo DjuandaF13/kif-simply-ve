78b2c542d79629e5731542e11f0be18d99bb930b
Fachriza Djuanda <79266141+DjuandaF13@users.noreply.github.com>
2026-05-26 11:51:11 +0700

    Update API base URL for production environment

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // File Konfigurasi Terpusat untuk Frontend
 
 // Menggunakan 'http://127.0.0.1:8000' untuk akses lokal di komputer yang sama.
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://djuandaf13-simply-ve-api.hf.space';
