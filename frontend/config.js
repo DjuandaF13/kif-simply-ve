c1d26aa2cd7310fb251cc1cf20941f2a7c0edb0f
Fachriza Djuanda <79266141+DjuandaF13@users.noreply.github.com>
2026-05-26 15:21:58 +0700

    Rename API_URL to API_BASE_URL in config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // File Konfigurasi Terpusat untuk Frontend
 
 // Menggunakan 'http://127.0.0.1:8000' untuk akses lokal di komputer yang sama.
-const API_URL = 'https://djuandaf13-simply-ve-api.hf.space';
+const API_BASE_URL = 'https://djuandaf13-simply-ve-api.hf.space';
